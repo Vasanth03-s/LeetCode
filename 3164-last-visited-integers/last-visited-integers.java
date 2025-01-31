ac77493293e683eeb3403b82094c59932f43032d
Vasanth S <157895520+Vasanth03-s@users.noreply.github.com>
2025-01-31 14:30:12 +0530

    Time: 1 ms (100.00%) | Memory: 44.7 MB (63.64%) - LeetSync

--- 3164-last-visited-integers/last-visited-integers.java
+++ 3164-last-visited-integers/last-visited-integers.java
@@ -2,17 +2,17 @@ class Solution {
     public List<Integer> lastVisitedIntegers(int[] nums) {
         List<Integer> ans = new ArrayList<>();
         List<Integer> seen = new ArrayList<>();
-        int k = 0;
-
-        for (int num : nums) {
-            if (num > 0) {
-                seen.add(0, num);
+        int k=0;
+        for (int num:nums) {
+            if (num>0) {
+                seen.add(0,num);
                 k=0;
             } else if (num==-1) {
                 k++;
                 if (k<=seen.size()) {
-                    ans.add(seen.get(k - 1));
-                } else {
+                    ans.add(seen.get(k-1));
+                } 
+                else{
                     ans.add(-1);
                 }
             }
