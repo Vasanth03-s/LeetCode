66ac188d9bb7fd694224ee74cb5c224497bff4eb
Vasanth S <157895520+Vasanth03-s@users.noreply.github.com>
2025-02-18 13:56:58 +0530

    Time: 1268 ms (9.30%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,8 +1,9 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-DECLARE M INT;
-SET M=N-1;
-RETURN (
-SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT M, 1
-);
+
+SET N=N-1;
+  RETURN (
+      SELECT DISTINCT salary FROM Employee ORDER BY salary DESC 
+      LIMIT 1 OFFSET N      
+  );
 END
