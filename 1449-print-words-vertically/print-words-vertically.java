f2dc2d33357646399be19dd48b2ea23c65a0ca0b
Vasanth S <157895520+Vasanth03-s@users.noreply.github.com>
2025-04-05 14:19:41 +0530

    Time: 3 ms (22.91%) | Memory: 42.2 MB (11.45%) - LeetSync

---
+++ 1449-print-words-vertically/print-words-vertically.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> printVertically(String s) {
+        ArrayList<String> result = new ArrayList<>();
+        String[] words = s.split(" ");
+        int max= 0;
+        for (String word : words) {
+            max = Math.max(max,word.length());
+        }
+        for (int i = 0; i < max; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (String word : words) {
+                if (i < word.length()) {
+                    sb.append(word.charAt(i));
+                } else {
+                    sb.append(" ");
+                }
+            }
+            String ts = sb.toString();
+            result.add(ts.replaceAll("\\s+$", ""));
+        }
+        return result;
+        
+    }
+}
